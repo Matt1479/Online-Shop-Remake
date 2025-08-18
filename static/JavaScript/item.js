3927086dd1ac6c26003c03bc4a3053d46b9edd36
Matt1479 <gitmatt1479@gmail.com>
2025-08-18 13:05:47 +0200

    Refactor/Fix(item.html. item.js): Use 1 for default value instead of item.qty.

--- static/JavaScript/item.js
+++ static/JavaScript/item.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     document.querySelector('ul#qtyDropdown').addEventListener('click', (event) => {
+
         document.querySelector('button#qtyDropdownTrigger')
             .textContent = 'Quantity: ' + event.target.textContent;
         document.querySelector('input[name="qty"]').value = event.target
             .textContent.trim();
+        
     });
+
 });
